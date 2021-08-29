08cbac386ce611f0418d0153cbe7589c67b165be
ZerolCamb <64336142+ZerolCamb@users.noreply.github.com>
2021-08-29 02:56:30 -0500

    Encendido de LED con la Raspberry (Con curita)
    SE tiene un "Curita" en forma de utilizar una funcion "Esperar" extra, pues la primera secuencia no dura 5 segundos sino 4.8s

--- EncendidoDeLed_Cesar/CAMB_EncendidoDeLedRASP.c
+++ EncendidoDeLed_Cesar/CAMB_EncendidoDeLedRASP.c
@@ -6,10 +6,10 @@
  * cambiar dependiendo del circuito del que utilice este programa
  * 
  * Componente     Pin Raspberry Pi 3     Estados lógicos
- * led 1-------GPIO 5---------------------On=>HIGH, Off=>LOW
- * led 2-------GPIO 6---------------------On=>HIGH, Off=>LOW
- * led 3-------GPIO 13--------------------On=>HIGH, Off=>LOW
- * led 4-------GPIO 19--------------------On=>HIGH, Off=>LOW
+ * led 0-------GPIO 5---------------------On=>HIGH, Off=>LOW
+ * led 1-------GPIO 6---------------------On=>HIGH, Off=>LOW
+ * led 2-------GPIO 13--------------------On=>HIGH, Off=>LOW
+ * led 3-------GPIO 19--------------------On=>HIGH, Off=>LOW
  */
 
 /*Se incluyen las librerias*/
@@ -24,13 +24,15 @@
 #define led2 23
 #define led3 24
 
-unsigned long cincoseg, espacio; /*Variables para trabajar con el tiempo*/
+float cincoseg, espacio; /*Variables para trabajar con el tiempo*/
 
 /*Se declaran las funciones*/
 void esperar();
 void apagar();
+void encender();
 void ascendente();
 void aditiva();
+void aditiva_des();
 void descendente();
 int main(void)
 {
@@ -42,13 +44,16 @@ int main(void)
 	pinMode(led2,OUTPUT);
 	pinMode(led3,OUTPUT);
 	
-	apagar();
 	ascendente();
 	apagar();
+	esperar(); //Curita
 	aditiva();
+	apagar();
+	aditiva_des();
 	apagar();
 	descendente();
 	apagar();
+	
 	return 0;
 }
 
@@ -58,6 +63,7 @@ int main(void)
 void esperar(){
 	espacio = millis();
 	while(millis() < espacio+entre);
+	printf("Pasaron %.2f segundos\n", espacio/1000);
 }
 /*Funcion apagar, se apagan todos los leds*/
 void apagar(){
@@ -67,11 +73,20 @@ void apagar(){
 	digitalWrite(led2, LOW);
 	digitalWrite(led3, LOW);
 }
+void encender(){
+	/*Se encienden todos los leds*/
+	digitalWrite(led0, HIGH);
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+	digitalWrite(led3, HIGH);
+}
 /*Secuencia ascendente exclusiva*/
 void ascendente(){
+	printf("Secuencia ascendente\n");
 	cincoseg = millis();
 	while(millis() < cincoseg+tiempo){
 		digitalWrite(led3, HIGH);
+		printf("l3 %d\t", digitalRead(led3));
 		esperar();
 		digitalWrite(led3, LOW);
 		digitalWrite(led2, HIGH);
@@ -83,10 +98,12 @@ void ascendente(){
 		digitalWrite(led0, HIGH);
 		esperar();
 		digitalWrite(led0, LOW);
+		esperar();
 	}
 }
 /*Secuencia aditiva exclusiva*/
 void aditiva(){
+	printf("Secuencia aditiva\n");
 	cincoseg = millis();
 	while(millis() < cincoseg+tiempo){
 		digitalWrite(led3, HIGH);
@@ -103,6 +120,7 @@ void aditiva(){
 }
 void descendente(){
 	/*Secuencia descendente exclusiva*/
+	printf("Secuencia descendente\n");
 	cincoseg = millis();
 	while(millis() < cincoseg+tiempo){
 		digitalWrite(led0, HIGH);
@@ -117,5 +135,23 @@ void descendente(){
 		digitalWrite(led3, HIGH);
 		esperar();
 		digitalWrite(led3, LOW);
+		esperar();
+	}
+}
+/*Secuencia descendente aditiva exclusiva*/
+void aditiva_des(){
+	printf("Secuencia descentende aditiva\n");
+	cincoseg = millis();
+	while(millis() < cincoseg+tiempo){
+		encender();
+		esperar();
+		digitalWrite(led0, LOW);
+		esperar();
+		digitalWrite(led1, LOW);
+		esperar();
+		digitalWrite(led2, LOW);
+		esperar();
+		digitalWrite(led3, LOW);
+		esperar();
 	}
 }
